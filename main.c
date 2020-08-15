e3d85a073cddd13ef7d441d843e9b377750f90e1
teamza09 <69686744+teamza09@users.noreply.github.com>
2020-08-15 21:26:05 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-  float a, b;
-  printf("Enter A : ");
-  scanf("%f", &a);
-  printf("Enter B : ");
-  scanf("%f", &b);
-  printf("%f + %f = %f", a, b, a+b);
-  return 0;
+float a, b;
+printf("Enter A : ");
+scanf("%f", &a);
+printf("Enter B : ");
+scanf("%f", &b);
+printf("%0.2f * %0.2f = %0.2f", a, b, a*b);
+return 0;
 }
